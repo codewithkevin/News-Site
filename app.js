f1251940882497e3bf405cb52a4e435662cb0875
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-20 22:17:59 +0000

    update

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.post ("/", function(req, res){
                 email_address: email,
                 status: "subscribed",
                 merge_fields: {
-                    FNAME: "firstName",
-                    LNAME: "lastName"
+                    FNAME: firstName,
+                    LNAME: lastName
                 }
             }
         ]
@@ -45,14 +45,14 @@ app.listen(3000, function () {
 // de43a02608aff9c8cdfe9e667533e2e5-us14
 
 //**Audice */
-ea8e470090
+// ea8e470090
 
-//**Update */
-const mailchimpClient = require("mailchimp_transactional")("YOUR_API_KEY");
+// //**Update */
+// const mailchimpClient = require("mailchimp_transactional")("YOUR_API_KEY");
 
-const run = async () => {
-  const response = await mailchimpClient.templates.update({ name: "name" });
-  console.log(response);
-};
+// const run = async () => {
+//   const response = await mailchimpClient.templates.update({ name: "name" });
+//   console.log(response);
+// };
 
-run();
+// run();
