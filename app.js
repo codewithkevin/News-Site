ec51c92e9985d182f6c240bfe9c034efea21077c
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-20 16:15:08 +0000

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 const app = express();
 
-
+app.use(express.static("public"));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/singup.html');
