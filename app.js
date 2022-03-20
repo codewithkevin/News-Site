a507e6b5f53c5fa1c81a10e915fe81bae0a05b92
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-20 14:25:37 +0000

    update

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const request = require('request');
-
-// const app = express();
-
-
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + '/../index.html');
-// })
-
-
-// app.listen(3000, function () {
-//    console.log('Server is running on port 300') 
-// })
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
