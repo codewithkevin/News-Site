efe8acd25c7c4ecf9179d9605f92f2f50b608af5
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-20 14:25:21 +0000

    update

--- app.js
+++ app.js
@@ -18,3 +18,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
+const app = express();
+
+app.listen(3000, function () {
+    console.log('Server is running on port 300')
+})
