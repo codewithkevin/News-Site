d5cf69e467cda2ece9655851fbf66ca5f4a4f173
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-20 16:06:36 +0000

    update

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const request = require('request');
 
 const app = express();
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/signup.html');
+});
+
 app.listen(3000, function () {
     console.log('Server is running on port 300')
 })
