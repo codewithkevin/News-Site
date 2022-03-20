3fa0adfdf1239fac0377cd0fedba0f4147c39ddc
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-20 16:19:15 +0000

    update

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const request = require('request');
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/singup.html');
     
 });
+
+app.post ("/", function(req, res){
+
+})
 
 app.listen(3000, function () {
     console.log('Server is running on port 300')
