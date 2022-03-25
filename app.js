ce555c16beb7793b40b3cdab0103754b14a93ca4
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-25 04:41:16 +0000

    update

--- app.js
+++ app.js
@@ -46,9 +46,7 @@ app.post ("/", function(req, res){
         if(response.statusCode === 200){
             res.sendFile(__dirname + "/success.html")
         }
-        else {
-            
-        }
+        
 
         response.on('data', function (data) {
             console.log(JSON.parse(data));
